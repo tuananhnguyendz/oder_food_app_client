551cd48900cd16f00d8f250bb9969f361489e326
Admin <k3.a4.tuananhh@gmail.com>
2024-03-07 20:38:22 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "oder_food_app_client"
+include(":app")
+ 
